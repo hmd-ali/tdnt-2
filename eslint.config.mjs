022a2a3ecfae8346a092b606309069359b5c5957
Ali Hammoud <letsmailali.h@gmail.com>
2025-01-20 16:59:54 +0200

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import withNuxt from "./.nuxt/eslint.config.mjs"
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+import tailwind from "eslint-plugin-tailwindcss"
+
+export default withNuxt(
+  // Your custom configs here
+  ...tailwind.configs["flat/recommended"],
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "tailwindcss/no-custom-classname": "off",
+    },
+  },
+)
